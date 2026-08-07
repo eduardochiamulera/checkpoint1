a5985b6ad72979a87f2df21aed85cdd0b2ac0834
eduardochiamulera <eduardofraga1994@gmail.com>
2026-08-07 20:23:00 -0300

    fix

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
