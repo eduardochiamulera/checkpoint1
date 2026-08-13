70bbe5899ca1a7acb7911476c1f7d6c21dfbe0fb
eduardochiamulera <eduardofraga1994@gmail.com>
2026-08-13 13:49:54 -0300

    feat: add Application DI and initial migration

---
+++ src/Cursos.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cursos.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Register MediatR handlers from this assembly
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+        
+        return services;
+    }
+}
