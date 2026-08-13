25777b22d2c1e965b4da53596da06b64ec794fd5
eduardochiamulera <eduardofraga1994@gmail.com>
2026-08-13 17:11:15 -0300

    add namespaces

--- src/Cursos.API/Middleware/LoggingMiddleware.cs
+++ src/Cursos.API/Middleware/LoggingMiddleware.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Cursos.API.Middleware;
 
--- src/Cursos.Infrastructure/DependencyInjection.cs
+++ src/Cursos.Infrastructure/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using System;
 using Cursos.Domain.Interfaces;
 using Cursos.Domain.Payments;
 using Cursos.Infrastructure.Data;
